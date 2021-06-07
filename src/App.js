5d91381e802fdb3ad9cb230b2352b1320ce4920b
gonzalbi <gonza-martin@hotmail.com>
2021-06-07 00:26:38 -0300

    connect with backend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import './App.css';
 import HomeScreen from './Screens/HomeScreen'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 function App() {
   return (
--- src/Components/IGDAManager.js
+++ src/Components/IGDAManager.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Button from 'react-bootstrap/Button';
 
 class IGDAManager extends Component {
     constructor(){
@@ -15,11 +16,27 @@ class IGDAManager extends Component {
             <div>
                 <div>
                 <h2>IGDA Manager</h2>
-                <p>
-                    {
-                    this.state.IGDAData.map(data => <p>{data.toString()}</p>)
-                    }
-                </p>
+                {
+                    this.state.IGDAData.map(data => 
+                        <div class="location-container">
+                        <div class="location-text" id={"location-id-"+data.Id} location-id={data.Id}>Locacion: {data.Name}</div>
+                        {data.Sectors.map(sector => 
+                            <div class="sector-container">
+                            <div class="sector-text" id={"sector-id-"+sector.Id} sector-id={sector.Id}>Sector : {sector.Name}</div>
+                            {
+                                sector.SubSectors.map(subsector => 
+                                    <div class="subsector-text" id={"subsector-id-"+subsector.Id} subsector-id={subsector.Id}>SubSector: {subsector.Name}</div>
+                                )
+                            }
+                            </div>
+                        )}
+                        <div>
+                            <Button variant="primary">Edit</Button>{' '}
+                            <Button variant="danger">Delete</Button>{' '}
+                        </div>
+                        </div>
+                    )
+                }
                 </div>
             </div>
         );
